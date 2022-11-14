52de2384b874f27173ea409bbf7a7e4c77dfadff
TRASALBY <ghtjd8264@naver.com>
2022-11-14 18:57:27 +0900

    chore : Dependency 추가

--- app/src/main/java/com/lateinit/rightweight/ui/home/HomeActivity.kt
+++ app/src/main/java/com/lateinit/rightweight/ui/home/HomeActivity.kt
@@ -7,6 +7,6 @@ import com.lateinit.rightweight.R
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
