d3582a8f417326d6361a4367c0d580ac9007eddc
jaeseuk <sjs4126@naver.com>
2022-11-16 17:19:52 +0900

    Issue #7 feat: 루틴 관련 엔티티 추가

---
+++ app/src/main/java/com/lateinit/rightweight/data/database/entity/Day.kt
@@ -0,0 +1,28 @@
+package com.lateinit.rightweight.data.database.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.ForeignKey.CASCADE
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "day",
+    foreignKeys = [
+        ForeignKey(
+            entity = Routine::class,
+            parentColumns = ["routine_id"],
+            childColumns = ["routine_id"],
+            onDelete = CASCADE
+        )
+    ]
+)
+data class Day(
+    @PrimaryKey
+    @ColumnInfo(name = "day_id")
+    val dayId: String,
+    @ColumnInfo(name = "routine_id")
+    val routineId: String,
+    @ColumnInfo(name = "order")
+    val order: Long,
+)
