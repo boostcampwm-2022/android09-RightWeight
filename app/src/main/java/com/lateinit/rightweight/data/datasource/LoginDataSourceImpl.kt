585b2b92627398205a68069828d2a5d5ccb41148
sky8492002 <sky8492002@gmail.com>
2022-11-16 18:11:27 +0900

    Issue #9 feat: 로그인 요청 시 네트워크 예외를 처리하고 Activity에서 요청 결과를 확인할 수 있도록 구현

--- app/src/main/java/com/lateinit/rightweight/data/datasource/LoginDataSourceImpl.kt
+++ app/src/main/java/com/lateinit/rightweight/data/datasource/LoginDataSourceImpl.kt
@@ -2,19 +2,20 @@ package com.lateinit.rightweight.data.datasource
 
 import android.util.Log
 import com.google.gson.Gson
+import com.lateinit.rightweight.data.LoginResponse
 import com.lateinit.rightweight.data.RightWeightRetrofitService
 import javax.inject.Inject
+import kotlin.math.log
 
 class LoginDataSourceImpl @Inject constructor(
     val api: RightWeightRetrofitService
 ) : LoginDataSource {
 
-    override suspend fun loginToFirebase(key: String, token: String){
-
+    override suspend fun loginToFirebase(key: String, token: String): LoginResponse {
         val postBody = PostBody(token, "google.com").toString()
-
-        val loginResponse = api.loginToFirebase(key, LoginRequestBody(postBody, "http://localhost", true, true))
-        Log.d("loginResponse", loginResponse.toString())
+        val loginResponse =
+            api.loginToFirebase(key, LoginRequestBody(postBody, "http://localhost", true, true))
+        return loginResponse
     }
 }
 
@@ -28,7 +29,7 @@ data class LoginRequestBody(
 data class PostBody(
     val id_token: String,
     val providerId: String,
-){
+) {
     override fun toString(): String {
         return "id_token=$id_token&providerId=$providerId"
     }
--- app/src/main/java/com/lateinit/rightweight/ui/login/LoginActivity.kt
+++ app/src/main/java/com/lateinit/rightweight/ui/login/LoginActivity.kt
@@ -1,18 +1,26 @@
 package com.lateinit.rightweight.ui.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
+import com.google.android.material.snackbar.Snackbar
 import com.lateinit.rightweight.R
 import com.lateinit.rightweight.databinding.ActivityLoginBinding
+import com.lateinit.rightweight.ui.home.HomeActivity
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import kotlin.math.log
 
 @AndroidEntryPoint
 class LoginActivity : AppCompatActivity() {
@@ -59,6 +67,26 @@ class LoginActivity : AppCompatActivity() {
     fun loginToFireBase(idToken: String?) {
         idToken?.let{
             viewModel.loginToFirebase(getString(R.string.google_api_key), idToken)
+
+            lifecycleScope.launch(){
+                viewModel.networkResult.collect(){ networkResult ->
+                    Log.d("NetworkResult", networkResult.toString())
+                    when(networkResult){
+                        NetworkState.NO_ERROR ->{
+                            viewModel.loginResponse.collect(){ loginResponse ->
+                                val nickname = loginResponse?.fullName.toString()
+                                Snackbar.make(binding.root, nickname, Snackbar.LENGTH_LONG). show()
+                                val intent = Intent(baseContext, HomeActivity::class.java)
+                                //startActivity(intent)
+                            }
+                        }
+                        NetworkState.WRONG_CONNECTION ->{
+                            Snackbar.make(binding.root, "인터넷 연결 오류", Snackbar.LENGTH_LONG). show()
+                        }
+                        else -> {}
+                    }
+                }
+            }
         }
     }
 }
