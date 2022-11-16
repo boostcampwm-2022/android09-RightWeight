f9ba4951ee0b24ae1a46d92b334cef2240ad2e42
Max <>
2022-11-16 17:35:37 +0900

    Issue #5 feat: HomeActivity 레이아웃 설정 및 바텀 네비게이션 구현
    Co-authored-by: TRASALBY <ghtjd264@naver.com>

--- app/src/main/java/com/lateinit/rightweight/ui/share/SharedRoutineFragment.kt
+++ app/src/main/java/com/lateinit/rightweight/ui/share/SharedRoutineFragment.kt
@@ -1,6 +1,7 @@
 package com.lateinit.rightweight.ui.share
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
