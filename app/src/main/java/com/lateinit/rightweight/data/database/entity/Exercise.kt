46e565c2ac25f4e0857f81134086debd5823f86e
jaeseuk <sjs4126@naver.com>
2022-11-17 19:53:18 +0900

    Issue #15 feat: 루틴 관련 데이터 클래스 수정

--- app/src/main/java/com/lateinit/rightweight/data/database/entity/Exercise.kt
+++ app/src/main/java/com/lateinit/rightweight/data/database/entity/Exercise.kt
@@ -4,7 +4,6 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.ForeignKey.CASCADE
-import androidx.room.Ignore
 import androidx.room.PrimaryKey
 import com.lateinit.rightweight.data.ExercisePartType
 
@@ -19,18 +18,16 @@ import com.lateinit.rightweight.data.ExercisePartType
         )
     ]
 )
-data class Exercise @JvmOverloads constructor(
+data class Exercise(
     @PrimaryKey
     @ColumnInfo(name = "exercise_id")
     val exerciseId: String,
     @ColumnInfo(name = "day_id")
     val dayId: String,
     @ColumnInfo(name = "title")
-    var title: String = "",
+    var title: String,
     @ColumnInfo(name = "order")
-    val order: Long,
+    val order: Int,
     @ColumnInfo(name = "part")
-    val part: ExercisePartType = ExercisePartType.CHEST,
-    @Ignore
-    val exerciseSets: List<Set> = listOf()
+    val part: ExercisePartType
 )
