8c08d953527e672c456e7dad5bb4d1110d33adee
jaeseuk <sjs4126@naver.com>
2022-11-17 11:14:11 +0900

    Issue #8 feat: 루틴 생성 프래그먼트 초기 설정

--- app/src/main/java/com/lateinit/rightweight/ui/routine/editor/RoutineEditorFragment.kt
+++ app/src/main/java/com/lateinit/rightweight/ui/routine/editor/RoutineEditorFragment.kt
@@ -5,14 +5,33 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.lateinit.rightweight.R
+import androidx.fragment.app.viewModels
+import com.lateinit.rightweight.databinding.FragmentRoutineEditorBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class RoutineEditorFragment : Fragment() {
+
+    private var _binding: FragmentRoutineEditorBinding? = null
+    private val binding
+        get() = checkNotNull(_binding) { "binding was accessed outside of view lifecycle" }
+
+    private val viewModel: RoutineEditorViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_routine_editor, container, false)
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = FragmentRoutineEditorBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
     }
 }
