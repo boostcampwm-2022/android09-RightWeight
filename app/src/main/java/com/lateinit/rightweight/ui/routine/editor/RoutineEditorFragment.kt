30b5b03415e411c5961d453ca2f96630e89665b4
jaeseuk <sjs4126@naver.com>
2022-11-17 17:49:30 +0900

    Issue #8 feat: 루틴 Adapter 설정

--- app/src/main/java/com/lateinit/rightweight/ui/routine/editor/RoutineEditorFragment.kt
+++ app/src/main/java/com/lateinit/rightweight/ui/routine/editor/RoutineEditorFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.lateinit.rightweight.data.ExercisePartType
 import com.lateinit.rightweight.databinding.FragmentRoutineEditorBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -18,6 +19,8 @@ class RoutineEditorFragment : Fragment() {
 
     private val viewModel: RoutineEditorViewModel by viewModels()
 
+    private lateinit var routineDayAdapter: RoutineDayAdapter
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
@@ -28,6 +31,60 @@ class RoutineEditorFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setRoutineDayAdapter()
+        setRoutineDaysObserve()
+    }
+
+    private fun setRoutineDayAdapter() {
+        routineDayAdapter = RoutineDayAdapter(object : RoutineDayAdapter.RoutineEventListener {
+            override fun onDayAdd() {
+                viewModel.addDay()
+            }
+
+            override fun onDayRemove(position: Int) {
+                viewModel.removeDay(position)
+            }
+
+            override fun onDayMoveUp(position: Int) {
+                viewModel.moveUpDay(position)
+            }
+
+            override fun onDayMoveDown(position: Int) {
+                viewModel.moveDownDay(position)
+            }
+
+            override fun onExerciseAdd(position: Int) {
+                viewModel.addExercise(position)
+            }
+
+            override fun onExerciseRemove(dayId: String, position: Int) {
+                viewModel.removeExercise(dayId, position)
+            }
+
+            override fun onExercisePartChange(
+                dayId: String,
+                position: Int,
+                exercisePartType: ExercisePartType,
+            ) {
+                viewModel.changeExercisePart(dayId, position, exercisePartType)
+            }
+
+            override fun onSetAdd(exerciseId: String) {
+                viewModel.addExerciseSet(exerciseId)
+            }
+
+            override fun onSetRemove(exerciseId: String, position: Int) {
+                viewModel.removeExerciseSet(exerciseId, position)
+            }
+
+        })
+        binding.recyclerViewDay.adapter = routineDayAdapter
+    }
+
+    private fun setRoutineDaysObserve() {
+        viewModel.days.observe(viewLifecycleOwner) {
+            routineDayAdapter.submitList(it)
+        }
     }
 
     override fun onDestroyView() {
