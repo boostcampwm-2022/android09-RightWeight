52703e6c4bd0f83bf33bbc941d4bc430daea1dc4
Max <>
2022-11-21 18:05:03 +0900

    Issue #28 feat: FAB 추가

--- app/src/main/java/com/lateinit/rightweight/ui/home/HomeFragment.kt
+++ app/src/main/java/com/lateinit/rightweight/ui/home/HomeFragment.kt
@@ -4,25 +4,37 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.lateinit.rightweight.R
+import com.lateinit.rightweight.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding
+        get() = checkNotNull(_binding) { "binding was accessed outside of view lifecycle" }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val btn = view.findViewById<Button>(R.id.startEx)
 
-        btn.setOnClickListener {
+        binding.floatingActionButtonStartExercise.setOnClickListener {
             it.findNavController().navigate(R.id.action_navigation_home_to_navigation_exercise)
         }
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
     }
 }
