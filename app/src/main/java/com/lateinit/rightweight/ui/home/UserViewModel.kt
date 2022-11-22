180aa6e86dc3470102d86a327ef98bdd1da349f9
Max <>
2022-11-23 02:09:42 +0900

    Issue #24 feat: 뷰 모델 이름 변경

--- app/src/main/java/com/lateinit/rightweight/ui/home/UserViewModel.kt
+++ app/src/main/java/com/lateinit/rightweight/ui/home/UserViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.lateinit.rightweight.data.database.entity.Day
 import com.lateinit.rightweight.data.model.User
 import com.lateinit.rightweight.data.repository.RoutineRepository
 import com.lateinit.rightweight.data.repository.UserRepository
@@ -12,7 +13,7 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor(
+class UserViewModel @Inject constructor(
     private val userRepository: UserRepository,
     private val routineRepository: RoutineRepository
 ) : ViewModel() {
@@ -26,19 +27,28 @@ class HomeViewModel @Inject constructor(
         getUser()
     }
 
+    fun setUser(routineId: String) {
+        val user = userInfo.value ?: return
+        viewModelScope.launch {
+            val days: List<Day> = routineRepository.getDaysByRoutineId(routineId)
+            userRepository.setUser(User(user.userId, routineId, days[0].dayId))
+        }
+    }
+
     fun getUser() {
         viewModelScope.launch {
             val user = userRepository.getUser()
             _userInfo.postValue(user)
+            getRoutineById()
         }
     }
 
-    fun getRoutineById() {
-        val routineId = userInfo.value?.routineId ?: run {
-            _routineTitle.value = "루틴을 설정해주세요."
-            return
-        }
+    private fun getRoutineById() {
         viewModelScope.launch {
+            val routineId = userRepository.getUser().routineId ?: run {
+                _routineTitle.postValue("루틴을 설정해주세요.")
+                return@launch
+            }
             val routine = routineRepository.getRoutineById(routineId)
             _routineTitle.postValue(routine.title)
         }
