f3ccdbc9322612e6b21587ed9d8a0c05c01dcf4b
Max <>
2022-11-22 23:24:49 +0900

    Issue #24 feat: User Repository, Datasource 생성

---
+++ app/src/main/java/com/lateinit/rightweight/data/datasource/UserLocalDataSource.kt
@@ -0,0 +1,16 @@
+package com.lateinit.rightweight.data.datasource
+
+import com.lateinit.rightweight.data.database.AppSharedPreferences
+import com.lateinit.rightweight.data.model.User
+import javax.inject.Inject
+
+class UserLocalDataSource @Inject constructor(private val appSharedPreferences: AppSharedPreferences) :
+    UserDataSource {
+    override suspend fun setUser(user: User) {
+        appSharedPreferences.setUser(user)
+    }
+
+    override suspend fun getUser(): User {
+        return appSharedPreferences.getUser()
+    }
+}
