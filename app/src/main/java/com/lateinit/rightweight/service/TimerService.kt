29f7fb6d0ed5d8b6feaf9289d8cee44750284ceb
sky8492002 <sky8492002@gmail.com>
2022-11-22 17:00:44 +0900

    Issue #23 feat: foreground 에서 작동하는 notification 구현

--- app/src/main/java/com/lateinit/rightweight/service/TimerService.kt
+++ app/src/main/java/com/lateinit/rightweight/service/TimerService.kt
@@ -1,5 +1,6 @@
 package com.lateinit.rightweight.service
 
+import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.Service
@@ -7,12 +8,19 @@ import android.content.Intent
 import android.os.Build
 import android.os.IBinder
 import android.util.Log
+import android.widget.RemoteViews
+import androidx.core.app.NotificationCompat
+import com.lateinit.rightweight.R
 import java.util.*
 
 class TimerService: Service() {
     private var isTimerRunning = false
     private var timeCount = 0
     lateinit var timer: Timer
+    lateinit var foregroundUpdateTimer: Timer
+    lateinit var notificationManager: NotificationManager
+    lateinit var customNotification: Notification
+    lateinit var notificationLayout: RemoteViews
 
 
     override fun onBind(intent: Intent?): IBinder? {
@@ -20,14 +28,25 @@ class TimerService: Service() {
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        //createChannel()
+        createChannel()
+        setNotification()
 
         val action = intent?.getStringExtra("timer_manage_action")!!
 
         when (action) {
-            "start" -> startTimer()
-            "pause" -> pauseTimer()
-            "status" -> sendStatus()
+            "start" -> {
+                startTimer()
+            }
+            "pause" -> {
+                pauseTimer()
+            }
+            "status" -> {
+                sendStatus()
+            }
+            "stop" ->{
+                pauseTimer()
+                stopNotification()
+            }
         }
 
         return super.onStartCommand(intent, flags, startId)
@@ -43,9 +62,41 @@ class TimerService: Service() {
             )
             notificationChannel.setSound(null, null)
             notificationChannel.setShowBadge(true)
-            val notificationManager = getSystemService(NotificationManager::class.java)
+
+            notificationManager = getSystemService(NotificationManager::class.java)
             notificationManager.createNotificationChannel(notificationChannel)
         }
+    }
+
+    private fun setNotification(){
+        notificationLayout = RemoteViews(packageName, R.layout.notification_foreground)
+        notificationLayout.setTextViewText(R.id.text_view_notification_timer, timeCount.toString())
+
+        customNotification = NotificationCompat.Builder(this, "timer_notification")
+            .setSmallIcon(R.drawable.img_right_weight)
+            .setStyle(NotificationCompat.DecoratedCustomViewStyle())
+            .setCustomContentView(notificationLayout)
+            .setCustomBigContentView(notificationLayout)
+            .build()
+
+        startForeground(1, customNotification)
+
+        foregroundUpdateTimer = Timer()
+
+        foregroundUpdateTimer.scheduleAtFixedRate(object : TimerTask() {
+            override fun run() {
+                updateNotification()
+            }
+        }, 0, 1000)
+    }
+
+    private fun stopNotification(){
+        stopNotification()
+    }
+
+    private fun updateNotification(){
+        notificationLayout.setTextViewText(R.id.text_view_notification_timer, getTimeString(timeCount))
+        startForeground(1, customNotification)
     }
 
     private fun startTimer() {
@@ -76,6 +127,13 @@ class TimerService: Service() {
         statusIntent.putExtra("is_timer_running", isTimerRunning)
         statusIntent.putExtra("time_count", timeCount)
         sendBroadcast(statusIntent)
+    }
+
+    fun getTimeString(timeCount: Int): String{
+        val hours: Int = (timeCount / 60) / 60
+        val minutes: Int = timeCount / 60
+        val seconds: Int = timeCount % 60
+        return "${"%02d".format(hours)}:${"%02d".format(minutes)}:${"%02d".format(seconds)}"
     }
 
 }
