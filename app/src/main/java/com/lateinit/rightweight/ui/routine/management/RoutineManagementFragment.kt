2d5f08559cc907fda9a667562bc89c5ff1e9eb8f
Max <>
2022-11-24 16:28:21 +0900

    Issue #51 feat: Recyclerview 어댑터 추가

--- app/src/main/java/com/lateinit/rightweight/ui/routine/management/RoutineManagementFragment.kt
+++ app/src/main/java/com/lateinit/rightweight/ui/routine/management/RoutineManagementFragment.kt
@@ -6,16 +6,23 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.lateinit.rightweight.databinding.FragmentRoutineManagementBinding
 import com.lateinit.rightweight.ui.home.UserViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class RoutineManagementFragment : Fragment() {
 
     private var _binding: FragmentRoutineManagementBinding? = null
     private val binding
         get() = checkNotNull(_binding) { "binding was accessed outside of view lifecycle" }
     private val userViewModel: UserViewModel by activityViewModels()
+    private val routineManagementViewModel: RoutineManagementViewModel by viewModels()
+    private val routineManagementAdapter: RoutineManagementAdapter by lazy {
+        RoutineManagementAdapter()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,7 +37,17 @@ class RoutineManagementFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         setBinding()
+        setAdapter()
+        routineManagementViewModel.routines.observe(viewLifecycleOwner) {
+            routineManagementAdapter.submitList(it)
+        }
     }
+
+    override fun onResume() {
+        routineManagementViewModel.getRoutineList()
+        super.onResume()
+    }
+
 
     override fun onDestroyView() {
         _binding = null
@@ -41,6 +58,10 @@ class RoutineManagementFragment : Fragment() {
         binding.lifecycleOwner = viewLifecycleOwner
         binding.userViewModel = userViewModel
         binding.fragment = this
+    }
+
+    private fun setAdapter() {
+        binding.recyclerViewRoutineManagement.adapter = routineManagementAdapter
     }
 
     fun navigateTo(id: Int) {
