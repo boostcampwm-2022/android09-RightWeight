d157b1db7c3c7237a1a7ccf9b02119a9d644615b
jaeseuk <sjs4126@naver.com>
2022-11-24 22:29:46 +0900

    Issue #30 feat: 루틴 관련 엔티티 @Ignore 필드 제거, 모든 필드 불변으로 변경

--- app/src/main/java/com/lateinit/rightweight/data/database/entity/Day.kt
+++ app/src/main/java/com/lateinit/rightweight/data/database/entity/Day.kt
@@ -4,7 +4,6 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.ForeignKey.CASCADE
-import androidx.room.Ignore
 import androidx.room.PrimaryKey
 
 @Entity(
@@ -18,14 +17,12 @@ import androidx.room.PrimaryKey
         )
     ]
 )
-data class Day @JvmOverloads constructor(
+data class Day(
     @PrimaryKey
     @ColumnInfo(name = "day_id")
     val dayId: String,
     @ColumnInfo(name = "routine_id")
     val routineId: String,
     @ColumnInfo(name = "order")
-    val order: Int,
-    @Ignore
-    val exercises: List<Exercise> = emptyList()
+    val order: Int
 )
--- app/src/main/java/com/lateinit/rightweight/data/database/entity/Exercise.kt
+++ app/src/main/java/com/lateinit/rightweight/data/database/entity/Exercise.kt
@@ -4,7 +4,6 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.ForeignKey.CASCADE
-import androidx.room.Ignore
 import androidx.room.PrimaryKey
 import com.lateinit.rightweight.data.ExercisePartType
 
@@ -19,18 +18,16 @@ import com.lateinit.rightweight.data.ExercisePartType
         )
     ]
 )
-data class Exercise @JvmOverloads constructor(
+data class Exercise(
     @PrimaryKey
     @ColumnInfo(name = "exercise_id")
     val exerciseId: String,
     @ColumnInfo(name = "day_id")
     val dayId: String,
     @ColumnInfo(name = "title")
-    var title: String,
+    val title: String,
     @ColumnInfo(name = "order")
     val order: Int,
     @ColumnInfo(name = "part")
-    val part: ExercisePartType,
-    @Ignore
-    val exerciseSets: List<ExerciseSet> = emptyList()
+    val part: ExercisePartType
 )
