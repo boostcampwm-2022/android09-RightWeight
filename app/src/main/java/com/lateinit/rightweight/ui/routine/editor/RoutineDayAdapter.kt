0410f58424cb36c096e5c09a254444825596b400
TRASALBY <ghtjd8264@naver.com>
2022-11-24 19:28:12 +0900

    Issue #54 feature: 루틴 생성 adapter, item 변경
    Co-authored-by: jaeseuk <sjs4126@naver.com>

--- app/src/main/java/com/lateinit/rightweight/ui/routine/editor/RoutineDayAdapter.kt
+++ app/src/main/java/com/lateinit/rightweight/ui/routine/editor/RoutineDayAdapter.kt
@@ -3,20 +3,16 @@ package com.lateinit.rightweight.ui.routine.editor
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.appcompat.content.res.AppCompatResources.getDrawable
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.lateinit.rightweight.R
-import com.lateinit.rightweight.data.database.entity.Day
 import com.lateinit.rightweight.databinding.ItemDayBinding
+import com.lateinit.rightweight.ui.model.DayUiModel
 
 class RoutineDayAdapter(
     private val onClickDay: (Int) -> Unit
-) : ListAdapter<Day, RoutineDayAdapter.DayViewHolder>(diffUtil) {
-
-    private var lastPosition = -1
-    private var selectedPosition = 0
+) : ListAdapter<DayUiModel, RoutineDayAdapter.DayViewHolder>(diffUtil) {
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
@@ -27,15 +23,6 @@ class RoutineDayAdapter(
 
     override fun onBindViewHolder(holder: DayViewHolder, position: Int) {
         holder.bind(getItem(position))
-        if (selectedPosition == position) {
-            holder.itemView.apply {
-                background = getDrawable(context, R.drawable.bg_day_order_seleted)
-            }
-        } else {
-            holder.itemView.apply {
-                background = getDrawable(context, R.drawable.bg_day_order)
-            }
-        }
     }
 
     inner class DayViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
@@ -46,26 +33,22 @@ class RoutineDayAdapter(
         init {
             itemView.setOnClickListener {
                 onClickDay(layoutPosition)
-                lastPosition = selectedPosition
-                selectedPosition = layoutPosition
-                notifyItemChanged(lastPosition)
-                notifyItemChanged(selectedPosition)
             }
         }
 
-        fun bind(day: Day) {
-            binding.day = day
+        fun bind(dayUiModel: DayUiModel) {
+            binding.dayUiModel = dayUiModel
         }
 
     }
 
     companion object {
-        val diffUtil = object : DiffUtil.ItemCallback<Day>() {
-            override fun areItemsTheSame(oldItem: Day, newItem: Day): Boolean {
+        val diffUtil = object : DiffUtil.ItemCallback<DayUiModel>() {
+            override fun areItemsTheSame(oldItem: DayUiModel, newItem: DayUiModel): Boolean {
                 return oldItem.dayId == newItem.dayId
             }
 
-            override fun areContentsTheSame(oldItem: Day, newItem: Day): Boolean {
+            override fun areContentsTheSame(oldItem: DayUiModel, newItem: DayUiModel): Boolean {
                 return oldItem == newItem
             }
 
--- app/src/main/java/com/lateinit/rightweight/ui/routine/editor/RoutineSetAdapter.kt
+++ app/src/main/java/com/lateinit/rightweight/ui/routine/editor/RoutineSetAdapter.kt
@@ -6,19 +6,18 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.lateinit.rightweight.R
-import com.lateinit.rightweight.data.database.entity.ExerciseSet
 import com.lateinit.rightweight.databinding.ItemSetBinding
-
+import com.lateinit.rightweight.ui.model.ExerciseSetUiModel
 
 class RoutineSetAdapter(val exerciseEventListener: RoutineExerciseAdapter.ExerciseEventListener) :
-    ListAdapter<ExerciseSet, RoutineSetAdapter.SetViewHolder>(diffUtil) {
+    ListAdapter<ExerciseSetUiModel, RoutineSetAdapter.SetViewHolder>(diffUtil) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SetViewHolder {
         return SetViewHolder(parent)
     }
 
     override fun onBindViewHolder(holder: SetViewHolder, position: Int) {
-        holder.bind(getItem(position) ?: return)
+        holder.bind(getItem(position))
     }
 
     inner class SetViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
@@ -26,21 +25,21 @@ class RoutineSetAdapter(val exerciseEventListener: RoutineExerciseAdapter.Exerci
     ) {
         private val binding = ItemSetBinding.bind(itemView)
 
-        fun bind(exerciseSet: ExerciseSet) {
-            binding.set = exerciseSet
+        fun bind(exerciseSetUiModel: ExerciseSetUiModel) {
+            binding.exerciseSetUiModel = exerciseSetUiModel
             binding.buttonSetRemove.setOnClickListener {
-                exerciseEventListener.onSetRemove(exerciseSet.exerciseId, layoutPosition)
+                exerciseEventListener.onSetRemove(exerciseSetUiModel.exerciseId, layoutPosition)
             }
         }
     }
 
     companion object {
-        val diffUtil = object : DiffUtil.ItemCallback<ExerciseSet>() {
-            override fun areItemsTheSame(oldItem: ExerciseSet, newItem: ExerciseSet): Boolean {
+        val diffUtil = object : DiffUtil.ItemCallback<ExerciseSetUiModel>() {
+            override fun areItemsTheSame(oldItem: ExerciseSetUiModel, newItem: ExerciseSetUiModel): Boolean {
                 return oldItem.setId == newItem.setId
             }
 
-            override fun areContentsTheSame(oldItem: ExerciseSet, newItem: ExerciseSet): Boolean {
+            override fun areContentsTheSame(oldItem: ExerciseSetUiModel, newItem: ExerciseSetUiModel): Boolean {
                 return oldItem == newItem
             }
 
