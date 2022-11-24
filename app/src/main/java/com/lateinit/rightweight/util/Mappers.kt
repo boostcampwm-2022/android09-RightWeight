08955090377947d214bbf4e9a90f427c0931cd42
jaeseuk <sjs4126@naver.com>
2022-11-24 22:30:29 +0900

    Issue #30 feat: UiModel to Entity 매핑 함수 추가

--- app/src/main/java/com/lateinit/rightweight/util/Mappers.kt
+++ app/src/main/java/com/lateinit/rightweight/util/Mappers.kt
@@ -1,6 +1,7 @@
 package com.lateinit.rightweight.util
 
 import com.lateinit.rightweight.data.database.entity.Day
+import com.lateinit.rightweight.data.database.entity.Exercise
 import com.lateinit.rightweight.data.database.entity.ExerciseSet
 import com.lateinit.rightweight.data.database.intermediate.ExerciseWithSets
 import com.lateinit.rightweight.ui.model.DayUiModel
@@ -24,7 +25,7 @@ fun ExerciseWithSets.toExerciseUiModel(): ExerciseUiModel {
         title = exercise.title,
         order = exercise.order,
         part = exercise.part,
-        exerciseSets = this.sets.map { it.toExerciseSetUiModel() }
+        exerciseSets = sets.map { it.toExerciseSetUiModel() }
     )
 }
 
@@ -34,6 +35,34 @@ fun ExerciseSet.toExerciseSetUiModel(): ExerciseSetUiModel {
         exerciseId = exerciseId,
         weight = weight,
         count = count,
+        order = order
+    )
+}
+
+fun DayUiModel.toDay(): Day {
+    return Day(
+        dayId = dayId,
+        routineId = routineId,
+        order = order
+    )
+}
+
+fun ExerciseUiModel.toExercise(): Exercise {
+    return Exercise(
+        exerciseId = exerciseId,
+        dayId = dayId,
+        title =  title,
+        order =  order,
+        part = part
+    )
+}
+
+fun ExerciseSetUiModel.toExerciseSet(): ExerciseSet {
+    return ExerciseSet(
+        setId = setId,
+        exerciseId = exerciseId,
+        weight = weight.ifEmpty { DEFAULT_SET_WEIGHT },
+        count = count.ifEmpty { DEFAULT_SET_COUNT },
         order = order
     )
 }
