bd0fdb6f08f4b7ca7d445ef870a8b6b5afd543a8
Max <>
2022-11-24 15:09:20 +0900

    Issue #51 feat: RoutineManagement 데이터바인딩 설정

--- app/src/main/java/com/lateinit/rightweight/ui/routine/management/RoutineManagementFragment.kt
+++ app/src/main/java/com/lateinit/rightweight/ui/routine/management/RoutineManagementFragment.kt
@@ -4,33 +4,46 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
-import com.lateinit.rightweight.R
-import com.lateinit.rightweight.ui.home.HomeActivity
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.lateinit.rightweight.databinding.FragmentRoutineManagementBinding
+import com.lateinit.rightweight.ui.home.UserViewModel
 
 class RoutineManagementFragment : Fragment() {
+
+    private var _binding: FragmentRoutineManagementBinding? = null
+    private val binding
+        get() = checkNotNull(_binding) { "binding was accessed outside of view lifecycle" }
+    private val userViewModel: UserViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_routine_management, container, false)
+    ): View {
+        _binding = FragmentRoutineManagementBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val createBtn = view.findViewById<Button>(R.id.create)
-        val viewBtn = view.findViewById<Button>(R.id.view)
 
-        createBtn.setOnClickListener {
-            it.findNavController()
-                .navigate(R.id.action_navigation_routine_management_to_navigation_routine_editor)
-        }
-        viewBtn.setOnClickListener {
-            it.findNavController()
-                .navigate(R.id.action_navigation_routine_management_to_navigation_routine_detail)
-        }
+        setBinding()
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+
+    private fun setBinding() {
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.userViewModel = userViewModel
+        binding.fragment = this
+    }
+
+    fun navigateTo(id: Int) {
+        findNavController().navigate(id)
     }
 }
