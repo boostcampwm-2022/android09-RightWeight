08e6b90a9b32ebad7a6ae742a71bf3dbf74cc1fb
TRASALBY <ghtjd8264@naver.com>
2022-11-24 14:15:15 +0900

    Issue #55 fix: 중간데이터 Relation entity 수정
    Co-authored-by: jaeseuk <sjs4126@naver.com>

--- app/src/main/java/com/lateinit/rightweight/data/database/intermediate/ExerciseWithSets.kt
+++ app/src/main/java/com/lateinit/rightweight/data/database/intermediate/ExerciseWithSets.kt
@@ -1,14 +1,15 @@
 package com.lateinit.rightweight.data.database.intermediate
 
+import androidx.room.Embedded
 import androidx.room.Relation
 import com.lateinit.rightweight.data.database.entity.Exercise
 import com.lateinit.rightweight.data.database.entity.ExerciseSet
 
 data class ExerciseWithSets(
-    val exercise: Exercise,
+    @Embedded val exercise: Exercise,
 
     @Relation(
-        entity = Exercise::class,
+        entity = ExerciseSet::class,
         parentColumn = "exercise_id",
         entityColumn = "exercise_id"
     )
--- app/src/main/java/com/lateinit/rightweight/data/database/intermediate/RoutineWithDays.kt
+++ app/src/main/java/com/lateinit/rightweight/data/database/intermediate/RoutineWithDays.kt
@@ -1,13 +1,15 @@
 package com.lateinit.rightweight.data.database.intermediate
 
+import androidx.room.Embedded
 import androidx.room.Relation
+import com.lateinit.rightweight.data.database.entity.Day
 import com.lateinit.rightweight.data.database.entity.Routine
 
 data class RoutineWithDays(
-    val routine: Routine,
+    @Embedded val routine: Routine,
 
     @Relation(
-        entity = Routine::class,
+        entity = Day::class,
         parentColumn = "routine_id",
         entityColumn = "routine_id"
     )
