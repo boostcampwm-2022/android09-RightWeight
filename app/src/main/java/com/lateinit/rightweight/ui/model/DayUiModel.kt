328f82de923786beca09f2403f8c5400730ce610
TRASALBY <ghtjd8264@naver.com>
2022-11-24 17:53:27 +0900

    Issue #54 feat: DayUiModel select상태에 따른 background 변경
    Co-authored-by: jaeseuk <sjs4126@naver.com>

--- app/src/main/java/com/lateinit/rightweight/ui/model/DayUiModel.kt
+++ app/src/main/java/com/lateinit/rightweight/ui/model/DayUiModel.kt
@@ -4,5 +4,6 @@ data class DayUiModel(
     val dayId: String,
     val routineId: String,
     val order: Int,
+    val selected: Boolean,
     val exercises: List<ExerciseUiModel> = emptyList()
 )
---
+++ app/src/main/java/com/lateinit/rightweight/util/Consts.kt
@@ -0,0 +1,3 @@
+package com.lateinit.rightweight.util
+
+const val FIRST_DAY_POSITION = 0
