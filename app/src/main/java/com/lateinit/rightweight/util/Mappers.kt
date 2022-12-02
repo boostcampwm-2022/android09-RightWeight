69f67068f7b3f691abbb025b2cd85be48a2f07fb
sky8492002 <sky8492002@gmail.com>
2022-12-02 22:59:05 +0900

    Issue #83 feat: DetailResponse<SharedRoutineField>.toSharedRoutine()에서 modifiedDate 설정하는 기능 추가

--- app/src/main/java/com/lateinit/rightweight/util/Mappers.kt
+++ app/src/main/java/com/lateinit/rightweight/util/Mappers.kt
@@ -17,6 +17,7 @@ import com.lateinit.rightweight.ui.model.HistoryExerciseSetUiModel
 import com.lateinit.rightweight.ui.model.HistoryExerciseUiModel
 import com.lateinit.rightweight.ui.model.HistoryUiModel
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 fun Day.toDayUiModel(index: Int, exerciseWithSets: List<ExerciseWithSets>): DayUiModel {
     return DayUiModel(
@@ -63,12 +64,12 @@ fun HistoryWithHistoryExercises.toHistoryUiModel(): HistoryUiModel {
 
 fun HistoryExerciseWithHistorySets.toHistoryExerciseUiModel(): HistoryExerciseUiModel {
     return HistoryExerciseUiModel(
-       exerciseId = historyExercise.exerciseId,
-       historyId = historyExercise.historyId,
-       title = historyExercise.title,
-       order = historyExercise.order,
-       part = historyExercise.part,
-       exerciseSets = historySets.map { it.toHistoryExerciseSetUiModel() }
+        exerciseId = historyExercise.exerciseId,
+        historyId = historyExercise.historyId,
+        title = historyExercise.title,
+        order = historyExercise.order,
+        part = historyExercise.part,
+        exerciseSets = historySets.map { it.toHistoryExerciseSetUiModel() }
     )
 }
 
@@ -95,8 +96,8 @@ fun ExerciseUiModel.toExercise(): Exercise {
     return Exercise(
         exerciseId = exerciseId,
         dayId = dayId,
-        title =  title,
-        order =  order,
+        title = title,
+        order = order,
         part = part
     )
 }
@@ -111,13 +112,16 @@ fun ExerciseSetUiModel.toExerciseSet(): ExerciseSet {
     )
 }
 
-fun DetailResponse<SharedRoutineField>.toSharedRoutine(): SharedRoutine{
+fun DetailResponse<SharedRoutineField>.toSharedRoutine(): SharedRoutine {
     val splitedName = name.split("/")
+    val refinedModifiedDateString = fields.modifiedDate?.value?.replace("T", " ")?.replace("Z", "")
+    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
+    val modifiedDate = LocalDateTime.parse(refinedModifiedDateString, formatter)
     return SharedRoutine(
-        routineId = splitedName[splitedName.size-1],
+        routineId = splitedName[splitedName.size - 1],
         title = fields.title?.value.toString(),
         author = fields.author?.value.toString(),
         description = fields.description?.value.toString(),
-        modifiedDate = LocalDateTime.now()
+        modifiedDate = modifiedDate
     )
 }
