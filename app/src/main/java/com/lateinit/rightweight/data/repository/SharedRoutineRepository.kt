fefe6a3a7395daff0b004330fd076a71791081d2
sky8492002 <sky8492002@gmail.com>
2022-12-05 01:16:56 +0900

    Issue #100 feat: 특정 routine에 대하여 room에 캐싱된 day 목록이 있다면 day, exercise, set 목록을 서버에 새로 요청하지 않고 room에서 가져오는 기능 구현

--- app/src/main/java/com/lateinit/rightweight/data/repository/SharedRoutineRepository.kt
+++ app/src/main/java/com/lateinit/rightweight/data/repository/SharedRoutineRepository.kt
@@ -7,9 +7,5 @@ import kotlinx.coroutines.flow.Flow
 
 interface SharedRoutineRepository {
     suspend fun getSharedRoutinesByPaging(): Flow<PagingData<SharedRoutine>>
-//    suspend fun getSharedRoutineDays(routineId: String): Flow<List<SharedRoutineDay>>
-//    suspend fun getSharedRoutineExercises(routineId: String, dayId: String): Flow<List<SharedRoutineExercise>>
-//    suspend fun getSharedRoutineExerciseSets(routineId: String, dayId: String, exerciseId: String): Flow<List<SharedRoutineExerciseSet>>
-
     suspend fun getSharedRoutineDetail(routineId: String): Flow<SharedRoutineWithDays>
 }
--- app/src/main/java/com/lateinit/rightweight/data/repository/SharedRoutineRepositoryImpl.kt
+++ app/src/main/java/com/lateinit/rightweight/data/repository/SharedRoutineRepositoryImpl.kt
@@ -1,16 +1,11 @@
 package com.lateinit.rightweight.data.repository
 
-import android.util.Log
 import androidx.paging.PagingData
 import com.lateinit.rightweight.data.database.entity.SharedRoutine
-import com.lateinit.rightweight.data.database.intermediate.RoutineWithDays
-import com.lateinit.rightweight.data.database.intermediate.SharedRoutineDayWithExercises
 import com.lateinit.rightweight.data.database.intermediate.SharedRoutineWithDays
 import com.lateinit.rightweight.data.datasource.RoutineLocalDataSource
 import com.lateinit.rightweight.data.datasource.RoutineRemoteDataSource
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class SharedRoutineRepositoryImpl @Inject constructor(
@@ -22,6 +17,9 @@ class SharedRoutineRepositoryImpl @Inject constructor(
     }
 
     override suspend fun getSharedRoutineDetail(routineId: String): Flow<SharedRoutineWithDays> {
+        if(routineLocalDataSource.getDaysByRoutineId(routineId).isEmpty()){
+            return routineLocalDataSource.getSharedRoutineWithDaysByRoutineId(routineId)
+        }
         routineRemoteDataSource.getSharedRoutineDays(routineId).collect() { sharedRoutineDays ->
             sharedRoutineDays.forEach() { sharedRoutineDay ->
                 routineRemoteDataSource.getSharedRoutineExercises(routineId, sharedRoutineDay.dayId)
@@ -41,22 +39,4 @@ class SharedRoutineRepositoryImpl @Inject constructor(
         return routineLocalDataSource.getSharedRoutineWithDaysByRoutineId(routineId)
     }
 
-//    override suspend fun getSharedRoutineDays(routineId: String): Flow<List<SharedRoutineDay>> {
-//        return routineRemoteDataSource.getSharedRoutineDays(routineId)
-//    }
-
-//    override suspend fun getSharedRoutineExercises(
-//        routineId: String,
-//        dayId: String
-//    ): Flow<List<SharedRoutineExercise>> {
-//        return routineRemoteDataSource.getSharedRoutineExercises(routineId, dayId)
-//    }
-//
-//    override suspend fun getSharedRoutineExerciseSets(
-//        routineId: String,
-//        dayId: String,
-//        exerciseId: String
-//    ): Flow<List<SharedRoutineExerciseSet>> {
-//        return routineRemoteDataSource.getSharedRoutineExerciseSets(routineId, dayId, exerciseId)
-//    }
 }
