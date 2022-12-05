3f2eb522dfb516e5e24d1156bd6c030fd3bdc088
jaeseuk <sjs4126@naver.com>
2022-12-05 15:18:54 +0900

    Issue #92 feat: History 데이터 레이어 리팩토링, 기록 정보 Flow 반환 함수 추가

--- app/src/main/java/com/lateinit/rightweight/data/database/dao/HistoryDao.kt
+++ app/src/main/java/com/lateinit/rightweight/data/database/dao/HistoryDao.kt
@@ -1,7 +1,11 @@
 package com.lateinit.rightweight.data.database.dao
 
-import androidx.room.*
-import androidx.sqlite.db.SupportSQLiteQuery
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Transaction
+import androidx.room.Update
 import com.lateinit.rightweight.data.database.entity.History
 import com.lateinit.rightweight.data.database.entity.HistoryExercise
 import com.lateinit.rightweight.data.database.entity.HistorySet
@@ -20,50 +24,24 @@ interface HistoryDao {
     )
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertHistorySet(
-        set: HistorySet
-    )
+    suspend fun insertHistorySet(set: HistorySet)
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertHistoryExercise(
-        historyExercise: HistoryExercise
-    )
-
-    @Update
-    suspend fun updateHistory(
-        history: History
-    )
-
-    @Update
-    suspend fun updateHistorySet(
-        set: HistorySet
-    )
-
-    @Update
-    suspend fun updateHistoryExercise(
-        historyExercise: HistoryExercise
-    )
+    suspend fun insertHistoryExercise(historyExercise: HistoryExercise)
 
     @Query("SELECT * FROM history WHERE date = :localDate")
     fun loadHistoryByDate(localDate: LocalDate) : Flow<List<History>>
 
+    @Transaction
+    @Query("SELECT * FROM history WHERE date = :localDate")
+    fun getHistoryByDate(localDate: LocalDate) : Flow<HistoryWithHistoryExercises?>
+
+    @Transaction
     @Query("SELECT * FROM history WHERE date BETWEEN :startDate AND :endDate")
     fun getHistoryBetweenDate(
         startDate: LocalDate,
         endDate: LocalDate
     ): Flow<List<HistoryWithHistoryExercises>>
-
-    @Query("SELECT * FROM history_exercise WHERE history_id = :historyId")
-    fun getHistoryExercisesByHistoryId(historyId: String): Flow<List<HistoryExercise>>
-
-    @Query("SELECT * FROM history_set WHERE exercise_id = :exerciseId ORDER BY `order`")
-    fun getHistorySetsByHistoryExerciseId(exerciseId: String): Flow<List<HistorySet>>
-
-    @Query("DELETE FROM history_set WHERE set_id = :historySetId")
-    suspend fun removeHistorySet(historySetId: String)
-
-    @Query("DELETE FROM history_exercise WHERE exercise_id = :historyExerciseId")
-    suspend fun removeHistoryExercise(historyExerciseId: String)
 
     @Query("SELECT COALESCE(MAX(`order`), 0) FROM history_exercise")
     suspend fun getMaxHistoryExerciseOrder(): Int
@@ -71,6 +49,18 @@ interface HistoryDao {
     @Query("SELECT COALESCE(MAX(`order`), 0) FROM history_set")
     suspend fun getMaxHistorySetOrder(): Int
 
-    @RawQuery(observedEntities = [HistorySet::class])
-    fun verifyAllHistorySets(query: SupportSQLiteQuery): Boolean
+    @Update
+    suspend fun updateHistory(history: History)
+
+    @Update
+    suspend fun updateHistorySet(set: HistorySet)
+
+    @Update
+    suspend fun updateHistoryExercise(historyExercise: HistoryExercise)
+
+    @Query("DELETE FROM history_set WHERE set_id = :historySetId")
+    suspend fun removeHistorySet(historySetId: String)
+
+    @Query("DELETE FROM history_exercise WHERE exercise_id = :historyExerciseId")
+    suspend fun removeHistoryExercise(historyExerciseId: String)
 }
