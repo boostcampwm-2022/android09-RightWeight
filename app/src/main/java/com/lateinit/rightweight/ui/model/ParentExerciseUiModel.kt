94a1fc6a46287926cef9bd87255937aecfd72bb2
jaeseuk <sjs4126@naver.com>
2022-12-05 15:20:18 +0900

    Issue #92 feat: ParentExerciseUiModel의 title 필드 var로 변경

--- app/src/main/java/com/lateinit/rightweight/ui/model/ParentExerciseUiModel.kt
+++ app/src/main/java/com/lateinit/rightweight/ui/model/ParentExerciseUiModel.kt
@@ -3,7 +3,7 @@ package com.lateinit.rightweight.ui.model
 import com.lateinit.rightweight.data.ExercisePartType
 
 abstract class ParentExerciseUiModel(
-    open val title: String,
+    open var title: String,
     open val part: ExercisePartType,
     open val expanded: Boolean,
     open val exerciseSets: List<ParentExerciseSetUiModel>
