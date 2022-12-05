ee0f58e94590c0ed596221ddfc939bc1e34f4688
jaeseuk <sjs4126@naver.com>
2022-12-05 15:13:52 +0900

    Issue #92 feat: 운동 기록 UiModel to Entity Mapping 함수 추가

--- app/src/main/java/com/lateinit/rightweight/util/Mappers.kt
+++ app/src/main/java/com/lateinit/rightweight/util/Mappers.kt
@@ -3,6 +3,8 @@ package com.lateinit.rightweight.util
 import com.lateinit.rightweight.data.database.entity.Day
 import com.lateinit.rightweight.data.database.entity.Exercise
 import com.lateinit.rightweight.data.database.entity.ExerciseSet
+import com.lateinit.rightweight.data.database.entity.History
+import com.lateinit.rightweight.data.database.entity.HistoryExercise
 import com.lateinit.rightweight.data.database.entity.HistorySet
 import com.lateinit.rightweight.data.database.entity.SharedRoutine
 import com.lateinit.rightweight.data.database.intermediate.ExerciseWithSets
@@ -115,7 +117,7 @@ fun ExerciseSetUiModel.toExerciseSet(): ExerciseSet {
 fun DetailResponse<SharedRoutineField>.toSharedRoutine(): SharedRoutine {
     val splitedName = name.split("/")
     val refinedModifiedDateString = fields.modifiedDate?.value?.replace("T", " ")?.replace("Z", "")
-    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
+    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")
     val modifiedDate = LocalDateTime.parse(refinedModifiedDateString, formatter)
     return SharedRoutine(
         routineId = splitedName.last(),
@@ -123,5 +125,37 @@ fun DetailResponse<SharedRoutineField>.toSharedRoutine(): SharedRoutine {
         author = fields.author?.value.toString(),
         description = fields.description?.value.toString(),
         modifiedDate = modifiedDate
+    )
+}
+
+fun HistoryUiModel.toHistory(): History {
+    return History(
+        historyId = historyId,
+        date = date,
+        time = time,
+        routineTitle = routineTitle,
+        dayOrder = order,
+        completed = completed
+    )
+}
+
+fun HistoryExerciseUiModel.toHistoryExercise(): HistoryExercise {
+    return HistoryExercise(
+        exerciseId = exerciseId,
+        historyId = historyId,
+        title = title,
+        order = order,
+        part = part
+    )
+}
+
+fun HistoryExerciseSetUiModel.toHistorySet(): HistorySet {
+    return HistorySet(
+        setId = setId,
+        exerciseId = exerciseId,
+        weight = weight.ifEmpty { DEFAULT_SET_WEIGHT },
+        count = count.ifEmpty { DEFAULT_SET_COUNT },
+        order = order,
+        checked = checked
     )
 }
