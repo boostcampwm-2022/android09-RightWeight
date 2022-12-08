16a73a3336aa699debc3c94a3b2572dbb86306c4
sky8492002 <sky8492002@gmail.com>
2022-12-08 15:03:31 +0900

    Issue #116 refactor: 불필요한 import 제거

--- app/src/main/java/com/lateinit/rightweight/ui/share/detail/SharedRoutineDetailFragment.kt
+++ app/src/main/java/com/lateinit/rightweight/ui/share/detail/SharedRoutineDetailFragment.kt
@@ -6,16 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import com.lateinit.rightweight.databinding.FragmentSharedRoutineDetailBinding
 import com.lateinit.rightweight.ui.model.DayUiModel
 import com.lateinit.rightweight.ui.routine.detail.DetailExerciseAdapter
 import com.lateinit.rightweight.ui.routine.editor.RoutineDayAdapter
 import com.lateinit.rightweight.util.collectOnLifecycle
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class SharedRoutineDetailFragment : Fragment() {
