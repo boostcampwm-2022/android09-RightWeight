43a1eb8773f21b895015b6e13d1e39e1da7630ce
Max <>
2022-12-09 08:49:34 +0900

    Issue #148 fix: 스낵바 루틴 상세페이지 이동 액션 제거

--- app/src/main/java/com/lateinit/rightweight/ui/share/SharedRoutineFragment.kt
+++ app/src/main/java/com/lateinit/rightweight/ui/share/SharedRoutineFragment.kt
@@ -2,9 +2,6 @@ package com.lateinit.rightweight.ui.share
 
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
@@ -15,10 +12,8 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
 import com.google.android.material.snackbar.Snackbar
-import com.lateinit.rightweight.NavGraphDirections
 import com.lateinit.rightweight.R
 import com.lateinit.rightweight.databinding.FragmentSharedRoutineBinding
 import com.lateinit.rightweight.ui.model.SharedRoutineSortTypeUiModel
@@ -40,15 +35,13 @@ class SharedRoutineFragment : Fragment(), SharedRoutineClickHandler {
         super.onCreate(savedInstanceState)
 
         setFragmentResultListener("routineCopy") { _, bundle ->
-            val routineId = bundle.getString("routineId") ?: return@setFragmentResultListener
             Snackbar.make(
                 binding.root,
                 R.string.success_save_routine,
                 Snackbar.LENGTH_SHORT
             ).apply {
-                setAction(getText(R.string.go_routine_detail)) {
-                    val action = NavGraphDirections.toNavigationRoutineDetail(routineId)
-                    it.findNavController().navigate(action)
+                setAction(getText(R.string.submit)) {
+                    this.dismiss()
                 }
             }.show()
         }
