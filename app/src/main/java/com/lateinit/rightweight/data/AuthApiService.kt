b8b453272ccc107de7960bede0e691493bbc7733
TRASALBY <ghtjd8264@naver.com>
2022-12-09 23:08:33 +0900

    Issue #154 refactor: AuthApiService

--- app/src/main/java/com/lateinit/rightweight/data/AuthApiService.kt
+++ app/src/main/java/com/lateinit/rightweight/data/AuthApiService.kt
@@ -1,7 +1,11 @@
 package com.lateinit.rightweight.data
 
-import com.lateinit.rightweight.data.datasource.LoginRequestBody
-import retrofit2.http.*
+import com.lateinit.rightweight.data.model.LoginRequestBody
+import retrofit2.http.Body
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface AuthApiService {
 
