0c5f9b2583bb04de54bbdd2cc25b3359f2b90f6e
TRASALBY <ghtjd8264@naver.com>
2022-12-11 02:42:01 +0900

    Issue #172 feat: 각 field값을 entity의 형태로 변환

--- app/src/main/java/com/lateinit/rightweight/data/datasource/remote/RoutineRemoteDataSource.kt
+++ app/src/main/java/com/lateinit/rightweight/data/datasource/remote/RoutineRemoteDataSource.kt
@@ -1,10 +1,22 @@
 package com.lateinit.rightweight.data.datasource.remote
 
+import com.lateinit.rightweight.data.database.entity.Day
+import com.lateinit.rightweight.data.database.entity.Exercise
+import com.lateinit.rightweight.data.database.entity.ExerciseSet
+import com.lateinit.rightweight.data.database.entity.Routine
 import com.lateinit.rightweight.data.model.remote.DocumentResponse
 import com.lateinit.rightweight.data.remote.model.RoutineField
 
 interface RoutineRemoteDataSource {
     suspend fun getRoutineByUserId(userId: String): List<DocumentResponse<RoutineField>>
+
+    suspend fun getRoutine(routineId: String, order: Int): Routine
+
+    suspend fun getRoutineDays(routineId: String): List<Day>?
+
+    suspend fun getRoutineExercises(path: String): List<Exercise>?
+
+    suspend fun getRoutineExerciseSets(path: String): List<ExerciseSet>?
 
     suspend fun getChildrenDocumentName(path: String): List<String>
 }
