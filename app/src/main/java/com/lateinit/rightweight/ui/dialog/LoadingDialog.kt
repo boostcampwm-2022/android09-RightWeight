84e73658ef3b35d001e885e7749b49e83dc8abef
TRASALBY <ghtjd8264@naver.com>
2022-12-12 19:16:33 +0900

    Issue #202 feat: LoadingDialog 생성 함수 구현

---
+++ app/src/main/java/com/lateinit/rightweight/ui/dialog/LoadingDialog.kt
@@ -0,0 +1,20 @@
+package com.lateinit.rightweight.ui.dialog
+
+import android.app.Dialog
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.view.Window
+
+class LoadingDialogProvider {
+
+    fun provideLoadingDialog(context: Context, loadingLayout: Int): Dialog {
+        return Dialog(context).apply {
+            requestWindowFeature(Window.FEATURE_NO_TITLE)
+            setCanceledOnTouchOutside(false)
+            setCancelable(false)
+            window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            setContentView(loadingLayout)
+        }
+    }
+}
